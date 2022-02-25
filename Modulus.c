b5b14089b00a98d50e179f496e99c6f7e5c81b6b
Durgaprasad143850 <99956265+Durgaprasad143850@users.noreply.github.com>
2022-02-25 13:51:08 +0530

    #include<stdio.h> int main() {     int a,b,m;     scanf("%d",&a);     scanf("%d",&b);     if((1<=a<=1000)||(1<=b<=1000))     {         m=a%b;         printf("%d",m);     } }

---
+++ Modulus.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,m;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if((1<=a<=1000)||(1<=b<=1000))
+    {
+        m=a%b;
+        printf("%d",m);
+    }
+}
