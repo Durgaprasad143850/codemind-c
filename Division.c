852fa8ce59695dcde4387105ab6658e425d746ee
Durgaprasad143850 <99956265+Durgaprasad143850@users.noreply.github.com>
2022-02-25 13:52:09 +0530

    #include<stdio.h> int main() {     int a,b,d;     scanf("%d",&a);     scanf("%d",&b);     if((1<=a<=1000)||(1<=b<=1000))     {        d=a/b;        printf("%d",d);
        }
    }

---
+++ Division.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if((1<=a<=1000)||(1<=b<=1000))
+    {
+       d=a/b;
+       printf("%d",d);
+       
+    }
+}
